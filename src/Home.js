3c2182305ceb8888f1a8a677f81f541217e7f377
davidlamarcook <dcook1025@gmail.com>
2022-12-22 21:34:33 -0700

    products props

--- src/Home.js
+++ src/Home.js
@@ -19,45 +19,45 @@ function Home() {
                             image="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/910fKjb8awL.__AC_SX300_SY300_QL70_FMwebp_.jpg"
                         />
                         <Product 
-                            id="12321342" 
+                            id="12321342"
                             title="Apple AirPods Pro (2nd Generation) Wireless Earbuds, Up to 2X More Active Noise Cancelling"
                             price={199}
                             rating={4}
-                            image="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/61SUj2aKoEL.__AC_SY445_SX342_QL70_FMwebp_.jpg"
+                            image="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/61SUj2aKoEL._AC_SL1500_.jpg"
                         />
                     </div>
 
                     <div className="home__row">
                         <Product 
-                            id="12321343" 
-                            title="Voova Laptop Bag 15.6 15 14 Inch Briefcase, Expandable Computer Shoulder Messenger Bag, Business Travel College School-Gray"
-                            price={30}
-                            rating={3}
-                            image="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/910fKjb8awL.__AC_SX300_SY300_QL70_FMwebp_.jpg"
+                            id="12321343"
+                            title="Jordan Womens WMNS Air Jordan 1 MID SE DO6699 200"
+                            price={212.99}
+                            rating={4}
+                            image="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71CCjVEhkTL._AC_UL1500_.jpg"
                         />
                         <Product 
-                            id="12321344" 
-                            title="Voova Laptop Bag 15.6 15 14 Inch Briefcase, Expandable Computer Shoulder Messenger Bag, Business Travel College School-Gray"
-                            price={30}
+                            id="12321344"
+                            title="Amazon Essentials Men's Full-Zip Polar Fleece Jacket (Available in Big & Tall)"
+                            price={15}
                             rating={3}
-                            image="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/910fKjb8awL.__AC_SX300_SY300_QL70_FMwebp_.jpg"
+                            image="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/816R2GcSWUS._AC_UL1500_.jpg"
                         />
                         <Product 
-                            id="12321345" 
-                            title="Voova Laptop Bag 15.6 15 14 Inch Briefcase, Expandable Computer Shoulder Messenger Bag, Business Travel College School-Gray"
-                            price={30}
-                            rating={3}
-                            image="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/910fKjb8awL.__AC_SX300_SY300_QL70_FMwebp_.jpg"
+                            id="12321345"
+                            title="Foldable Metal Platform Bed Frame with Tool Free Setup, 14 Inches High, Twin XL, Black"
+                            price={27.43}
+                            rating={5}
+                            image="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71zpDaTJ1RL._AC_SL1500_.jpg"
                         />
                     </div>
 
                     <div className="home__row">
                         <Product 
-                            id="12321346" 
-                            title="Voova Laptop Bag 15.6 15 14 Inch Briefcase, Expandable Computer Shoulder Messenger Bag, Business Travel College School-Gray"
-                            price={30}
-                            rating={3}
-                            image="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/910fKjb8awL.__AC_SX300_SY300_QL70_FMwebp_.jpg"
+                            id="12321346"
+                            title="TV 50 inch 4-Series 4K UHD smart TV"
+                            price={289}
+                            rating={5}
+                            image="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/61IZcaEIt4L._AC_SL1000_.jpg"
                         />
                     </div>
                 </div>
--- src/Product.js
+++ src/Product.js
@@ -1,20 +1,20 @@
 import React from "react";
 import './Product.css'
 
-function Product(props){
+function Product({id, title, price, rating, image}){
     return(
         <div>
             <div className="product">
                 <div className="product__info">
-                    <p>{props.title}</p>
+                    <p>{title}</p>
                     <p className="product__price">
                         <small>$</small>
-                        <strong>{props.price}</strong>
+                        <strong>{price}</strong>
                     </p>
                     <div className="product__rating">⭐⭐</div>
                 </div>
 
-                <img src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/910fKjb8awL.__AC_SX300_SY300_QL70_FMwebp_.jpg" alt="product-image" />
+                <img src={image} alt="product-image" />
 
                 <button>Add to Basket</button>
             </div>
